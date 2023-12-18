2e112b9e28a88e323925474f6e9dc202fbed96c8
Syarif <syarifsakur@gmail.com>
2023-12-19 07:14:00 +0800

    8

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const port = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 app.use(akunRouter)
 
 app.listen(port, () => {
   console.log(`server berjalan di port ${port}`);
 });
+
+express.api = (req,res)=>{
+  app(req,res)
+}
