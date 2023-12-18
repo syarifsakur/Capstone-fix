0af96595e384f0c1701700ddb4fb4f35e2d47868
Syarif <syarifsakur@gmail.com>
2023-12-19 07:36:03 +0800

    10

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const functions = require('firebase-functions')
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 const akunRouter = require('./route/router')
