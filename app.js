3e9ad76d6ada1000519f9230cbfcb47f71496078
Syarif <syarifsakur@gmail.com>
2023-12-18 21:01:21 +0800

    tes 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const akunRouter = require('./route/router')
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(akunRouter)
+
+app.listen(port, () => {
+  console.log(`server berjalan di port ${port}`);
+});
