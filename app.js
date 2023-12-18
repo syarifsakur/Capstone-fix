694c85a6a60f6f331bdaff617f6b1f30b178f5ab
Syarif <syarifsakur@gmail.com>
2023-12-19 07:47:17 +0800

    11

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const functions = require('firebase-functions')
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 const akunRouter = require('./route/router')
-const port = 4000;
+// const port = 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(akunRouter)
 
-app.listen(port, () => {
-  console.log(`server berjalan di port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server berjalan di port ${port}`);
+// });
 
 exports.app = functions.https.onRequest(app);
